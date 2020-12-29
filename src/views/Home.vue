55d51047fd9706fca961f618edf967082db3150d
Andrey Makrushin <mag.ondrei@gmail.com>
2020-12-29 16:58:32 +0300

    Стекло

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,31 +4,34 @@
   <div class="title">Расходы по категориям</div>
   
 
-  <div class="plan">
-    
+  <div class="plan" style="padding-bottom:5px">
    <div class="head_box" v-for="i in categories" :key="i.id">
-    <div style="color:white; width:120px">{{i.dif}}</div>
+    <div :class="dgclass(i.dif)" v-html="i.dif.toLocaleString()"></div>
     <a href="#" class="linkpage">{{i.name}}</a>
     <div class="buttonplus">+</div>
   </div>
-
   </div>
+
+
   <div class="title">Итоги</div>
+    
+    <div class="plan">
+      
+      
 
-  <div class="head_box">
-    <div>Остаток</div>
-    <div>74 550,00 р.</div>
-  </div>
+      <div class="steklo" style="width:210px"> 
+      <div class="head_box "><div>Остаток</div><div>74 550,00 р.</div></div>
+      <div class="head_box "><div>Расходы</div><div>25 450,00 р.</div></div>
+      <div class="head_box "><div>Бюджет</div><div>100 000,00 р.</div></div>
+      </div>
 
-  <div class="head_box">
-    <div>Расходы</div>
-    <div>25 450,00 р.</div>
-  </div>
-
-  <div class="head_box">
-    <div>Бюджет</div>
-    <div>100 000,00 р.</div>
-  </div>
+      
+    </div>
+  <div class="title">Планы</div>
+  <div class="plan"></div>
+    
+    
+    
 
 </div>
   
@@ -40,11 +43,16 @@ export default {
     return {
       categories: [
         {name: 'Продукты', dif: 456, id: 1},
-        {name: 'Хоз товары', dif: 41560, id: 2},
+        {name: 'Хоз товары', dif: 0, id: 2},
         {name: 'Автомобиль', dif: 11000, id: 3},
-        {name: 'Развлечения', dif: 5, id: 4},
+        {name: 'Развлечения', dif: -5, id: 4},
         {name: 'Ремонт', dif: 456, id: 5}
       ]
+    }
+  },
+  methods:{
+    dgclass(dg){
+      return dg < 0 ? 'dgminus' : 'dgnormal'
     }
   }
 }
@@ -85,11 +93,40 @@ export default {
     padding-left: 4px;
     padding-right: 4px;
     font-weight: 700;
+    cursor: pointer;
 }
-.linkpage {
-  color: #ffffff;
-  width: 180px;
+.buttonplus:active {
+  color: white;
+  box-shadow: none;
 }
 
+.linkpage {
+    color: #ffffff;
+    width: 100%;
+    text-align: end;
+    margin-right: 10px;
+    border-bottom-style: dotted;
+    border-bottom-width: thin;
+    text-decoration: none;
+}
+
+.dgminus {
+  color: brown;
+}
+
+.dgnormal {
+  color: white;
+}
+
+.steklo {
+    border-style: inset;
+    border-width: thin;
+    border-radius: 4px;
+    padding: 3px;
+    /*background-color: #75bb88;*/
+    background: linear-gradient(180deg, rgba(255,255,255,0.21052170868347342) 0%, rgb(0 0 0 / 15%) 100%);
+
+    color: #d3f9de;
+}
 
 </style>
