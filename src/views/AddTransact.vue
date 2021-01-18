bd36001f0ed383e05fe787b0a1aad8b0b70dee0d
Andrey Makrushin <mag.ondrei@gmail.com>
2021-01-18 14:33:44 +0300

    Правочка

--- src/views/AddTransact.vue
+++ src/views/AddTransact.vue
@@ -4,34 +4,30 @@
 
 
             <div style="    display: flex; justify-content: space-between; min-height: 0 !important;">
-                <button class="buttonplus button_large" tabindex="5" @click="$router.push('/')"> <span class="el-icon-caret-left buttoicon"></span> Отмена</button>
-                <button class="buttonplus button_large" tabindex="3"> <span class="el-icon-circle-check buttoicon"></span> В чек</button>
-                <button class="buttonplus button_large" tabindex="3"> <span class="el-icon-circle-check buttoicon"></span> Сохранить</button>
-            </div>            
-
+                <button class="buttonplus button_large" tabindex="6" @click="$router.push('/')"> <span class="el-icon-caret-left buttoicon"></span> Отмена</button>
+                <button class="buttonplus button_large" tabindex="4"> <span class="el-icon-circle-check buttoicon"></span> Сохранить</button>
+                <button class="buttonplus button_large" tabindex="5"> <span class="el-icon-circle-plus-outline buttoicon"></span> В чек</button>
+                
+            </div>
         
             <div class="back">    
                 <div class="h2">Продукты</div>
-                <input type="date" style="text-align: end;" id="amount_date" v-model="amount_date" tabindex="4" autocomplete="off">
+                <input type="date" style="text-align: end;" id="amount_date" v-model="amount_date" tabindex="1" autocomplete="off">
             </div>
             
             <div class="back" style="display: block;">    
-                <input type="number" v-model="amount" id="amount" style="text-align:center" tabindex="1" placeholder="Стоимость" autocomplete="off">
+                <input type="number" v-model="amount" id="amount" style="text-align:center" tabindex="2" autofocus  placeholder="Стоимость" autocomplete="off">
                 <hr>
-                <input type="text" style="text-align: center;"  v-model="amount_comment" id="amount_comment" tabindex="2" placeholder="Ваш комментарий" autocomplete="off"/>
+                <input type="text" style="text-align: center;"  v-model="amount_comment" id="amount_comment" tabindex="3" placeholder="Ваш комментарий" autocomplete="off"/>
             </div>           
                         
         </div>
         <div class="title">Нажмите <span style="color:crimson;">Enter</span> чтобы сохранить. Или <span style="color:crimson;">Shift+Enter</span>, чтобы добавить в чек</div>
 
 
-        <div class="plan">                
-            <div style="    display: flex; justify-content: flex-end; min-height: 0 !important;">
-                <button class="buttonplus button_large" tabindex="3"> <span class="el-icon-circle-check buttoicon"></span> Сохранить чек</button>
-            </div>
-                    
-            <div class="back" style="display: block; padding: 1em;">
-                <div class="chek_summ">653 руб.</div>
+        <div class="plan"> 
+            <div class="chek_summ">653 руб.</div>
+            <div class="back" style="display: block; padding: 1em;">                
                 <div v-for="ch in cur_chek" :key="ch.id" class="chek_element"> {{ch.price}}  <span class="chek_text"> {{ch.text}} </span></div>                        
             </div>
                     
@@ -72,9 +68,9 @@ export default {
 
 <style>
 .chek_summ {
-    margin-bottom: 1em;
+    margin: 0.5em;
     font-size: x-large;
-    text-align: center;
+    text-align: end;
 }
 .chek_element {
     margin-bottom: 6px;
